799ee08e19d4db5e5ec1d6ab02a038b465ec8202
Pierce Lauring <pclauring@gmail.com>
2019-07-05 10:35:28 -0400

    Added Card Scriptable object

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public Sprite art;
+    public string cardDetail;
+    public string cardFlavor;
+    public string artist;
+
+}
